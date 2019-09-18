d07ebce7d5c2984dbe0640bccf3d8e9067a5d5ae
Preetom_Bhowmik <bits3dec@gmail.com>
2019-09-18 09:16:44 +0530

    [Graph] Implement DFS, BFS

---
+++ Graph/Traversal/src/DFS.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DFS {
+	int v;
+	LinkedList<Integer> adj[];
+	
+	public DFS(Graph g) {
+		this.v = g.v;
+		this.adj = g.adj;
+	}
+	
+	public void dfs(int s) {
+		boolean visited[] = new boolean[v];
+		dfsUtil(s, visited);
+ 	}
+	
+	private void dfsUtil(int u, boolean visited[]) {
+		System.out.println(u);
+		visited[u] = true;
+		
+	    Iterator<Integer> it = adj[u].listIterator();
+		while(it.hasNext() == true) {
+			int v = it.next();
+			if(visited[v] == false) {
+				dfsUtil(v, visited);
+			}
+		}
+	}
+}
